a6a198e4e08bb1cecd9988ec47eeed0d59d7be69
Pedro <pedro.phprado@gmail.com>
2023-11-18 13:06:08 -0300

    Feat: Finalizando projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["*{.html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        azul:{
+          claro: '#C5BFFF',
+          ecuro: '#061E3C',
+          hover: '#1057B0',
+        }
+      },
+      fontFamily:{
+        inter: ['Inter', 'sans serif']
+      },
+      keyframes:{
+        sino_kf:{
+          '0%, 100%':{
+            transform: 'rotate(-10deg)'
+          },
+          '50%':{
+            transform: 'rotate(10deg)'
+          },
+        }
+      },
+      animation:{
+        sino:'sino_kf 0.25s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 }
